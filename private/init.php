8973f90e32ca3ec874cfb416c398e8aec6a53bba
LACOUR Valentin <valentin.lacour@etud.u-picardie.fr>
2017-01-10 17:59:11 +0100

    Création de la classe "DataFormatter"
    Formate les données en objet à partir d'un tableau, convertis également les données dans le bon type

--- private/init.php
+++ private/init.php
@@ -8,6 +8,8 @@
 
 define('ABS_PATH',$_SERVER['DOCUMENT_ROOT']);
 define('URL', $_SERVER['HTTP_HOST']);
+define('PARAM_PATH', ABS_PATH.'/private/parameters');
+define('CONFIG_FILE_NAME','config');
 
 require_once ABS_PATH.'/private/functions/autoloader.php';
 require_once ABS_PATH.'/private/functions/linker.php';
