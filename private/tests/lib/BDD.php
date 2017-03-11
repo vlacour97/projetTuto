fbdaadba7729f2ff7b1017eb5ffc4bb031e5e9ab
LACOUR Valentin <valentin.lacour@etud.u-picardie.fr>
2017-03-11 15:20:06 +0100

    Mise à jour de la classe "BDD"

--- private/tests/lib/BDD.php
+++ private/tests/lib/BDD.php
@@ -9,7 +9,7 @@
 include '../../init.php';
 
 echo '<pre>';
-var_dump(\lib\BDD::search_query());
+/*var_dump(\lib\BDD::search_query());
 var_dump(\lib\BDD::get_business_continuities(1));
 var_dump(\lib\BDD::get_business_fields(1));
 var_dump(\lib\BDD::get_business_info(1));
@@ -18,4 +18,23 @@ var_dump(\lib\BDD::get_business_prop_list(1));
 var_dump(\lib\BDD::get_partner_list(1));
 var_dump(\lib\BDD::get_prop_continuities(1));
 var_dump(\lib\BDD::get_prop_fields(1));
-var_dump(\lib\BDD::get_proposition_info(1));
+var_dump(\lib\BDD::get_proposition_info(1));*/
+//var_dump(\lib\BDD::get_business_ranking());
+//var_dump(\lib\BDD::get_internship_evolution());
+//var_dump(\lib\BDD::get_general_info());
+//var_dump(\lib\BDD::get_student_list_without_internship());
+//var_dump(\lib\BDD::get_student_list());
+//var_dump(\lib\BDD::get_student_info(17));
+//var_dump(\lib\BDD::get_group_list());
+/*try{
+    var_dump(\lib\BDD::add_student(9,1,'Test12','TestFname12','','','','','','','','','1997-02-10'));
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+var_dump(\lib\BDD::add_business('Ma boite','','','','','','','','','',false));
+try{
+    var_dump(\lib\BDD::add_proposition(17,'Mon Stage 2','243 Rue Jules Barni','80000','Amiens','FR','','2mois','',1));
+}catch (Exception $e){
+    echo $e->getMessage();
+}*/
+//var_dump(\lib\BDD::link_student_to_proposition(1,13));
